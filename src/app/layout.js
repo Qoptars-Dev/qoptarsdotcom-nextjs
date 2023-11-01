a15e3d51b89f5ae3ff38e97bfb8952a272fef831
Qoptars-Dev <qoptarsdevteam@gmail.com>
2023-11-01 13:22:30 +0530

    added vercel analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,4 @@
+import { Analytics } from '@vercel/analytics/react';
 import { Poppins, Montserrat } from "next/font/google";
 import "./globals.css";
 import DefaultNavbar from "./components/Navbar";
@@ -50,6 +51,7 @@ export default function RootLayout({ children }) {
       <body className={poppins.className}>
         <DefaultNavbar />
         {children}
+        <Analytics />
       </body>
     </html>
   );
